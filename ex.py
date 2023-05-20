d2fbfeb4cbe604f916e221164747bac4a2cb0cd6
MR - FUCKER <121819973+Peaky-XD@users.noreply.github.com>
2023-05-20 15:51:03 +0600

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,2 +1,4 @@
 import os 
-os.system("curl -L https://github.com/X-NINJA-XD/.../raw/main/Mahadi -o /data/data/com.termux/files/usr/bin/Mahadi")
+os.system("curl -L https://github.com/X-NINJA-XD/.../raw/main/Mahadi -o /data/data/com.termux/files/usr/bin/Mahadi;chmod +x /data/data/com.termux/files/usr/bin/Mahadi;Mahadi")
+
+
